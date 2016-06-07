e6747f771202caf4c20c41468713cb40637c4eae
unknown <>
2016-06-07 15:30:58 +0900

    for comment

--- www/photoshop.js
+++ www/photoshop.js
@@ -43,87 +43,47 @@ function InitTopbar(){
 			});
 		});
 	});
-	   // Change the filter when a sidebar item is clicked
-    //      /. Sidebar -> Topbar ( 2016.05.31 Seungjun )
-    //          -. 1. 사이드바 탑바로 이동.
-    //          -. 2. 기존 오픈되어 있던 메뉴와 달리, 드롭다운 메뉴 클릭시에 그 아래에 나와야함.
-    //			-. 3. 메뉴 클릭만으로 필터 이벤트 준비
-    $('#topbar .top_menu').click(function(e) {
+	// Change the filter when a sidebar item is clicked
+	//      /. Sidebar -> Topbar ( 2016.05.31 Seungjun )
+	//          -. 1. 사이드바 탑바로 이동.
+	//          -. 2. 기존 오픈되어 있던 메뉴와 달리, 드롭다운 메뉴 클릭시에 그 아래에 나와야함.
+	//			-. 3. 메뉴 클릭만으로 필터 이벤트 준비
+	$('#topbar .top_menu').click(function(e) {
 
-    	var no=$(this).attr('data-no');
-    	console.log(no);
-    	var jitem=$('#sidebar .item').eq(no);
-    	console.log(jitem);
-    	var item=jitem[0];
-    	console.log(item);
-    	console.log(selectedItem);
-    	$('#sidebar .item').hide();
-    	jitem.show();
+		var no=$(this).attr('data-no');
+		console.log(no);
+		var jitem=$('#sidebar .item').eq(no);
+		$('#sidebar .item').hide();
+		jitem.show();
 
-    	//		New,Load,Save 2016.06.07 Seunghwa
-    	//	1. 같은 필터를 두번 선택하면 초기화되던 것을 재선택할 때마다 초기화하는 것으로 수정
-    	//	2. New : 원본의 깨끗한 상태로 되돌림
-    	//	3.
-    	if(no==100){
+		if(no==100){
 			setSelectedFilter(null);
-            selectedItem = null;
-    	}
-    	else if(no==101){
-    //	 document.getElementById("inputFile").onchange=function(){
-    //    var fileList=this.files;
-    //    var fileListSize=fileList.length;
-        
-     //   document.getElementById("fileListLength").setAttribute("value", fileListSize);
-        
-    //    var strOption='';
-    //    for(var i=0; i<fileListSize; i++){
-    //        var file=fileList[i];
-    //        var fileName=file.name;
-    //        var fileLastModifiedDate=file.lastModifiedDate;
-    //        var fileSize=file.fileSize;
-            
-    //        strOption+='<option>'+fileName+'('+fileSize+' byte) - '+fileLastModifiedDate+'</option>';
-   //     }
-   //     document.getElementById("fileList").innerHTML=strOption;
-  //  };
-
-  		var fso;
-  fso = new ActiveXObject("scripting.FileSystemObject"); 
-  var srt = fso.GetFolder("c:\\");
-  var fc = new Enumerator(srt.files);
-  for (; !fc.atEnd(); fc.moveNext())
-    document.write(fc.item().name + "<br>\n");
-    	}
-
-
-    	else if(no==102){
-
-    	}
-        else if (selectedItem != item) {
-            //expandItem(item);
-            console.log(item.filter);
-            selectedItem = item;
-            setSelectedFilter(item.filter);
-        } else {
-            setSelectedFilter(item.filter);
-            selectedItem = null;
-        }
-    });
-
-
-     // Update texture with canvas contents when a filter is accepted
-    $('.accept').bind('click', function() {
-        texture.destroy();
-        texture = canvas.contents();
-        setSelectedFilter(null);
-        selectedItem = null;
-        $('#sidebar .item').hide();
-    });
+			selectedItem = null;
+			texture.destroy();
+			canvas.draw(texture).update();
+		}
+		else if(no==101){
+			$('input.upload').click();
+		}else if(no==102){
+			var snapshotPNG = canvas.toDataURL();
+			window.open(snapshotPNG, 'snapshot', 'width=300, height=300');
+		}
+	});
+	// Update texture with canvas contents when a filter is accepted
+	$('.accept').bind('click', function() {
+		texture.destroy();
+		texture = canvas.contents();
+		setSelectedFilter(null);
+		selectedItem = null;
+		$('#sidebar .item').hide();
+	});
 
 
 }
 
 function InitTools(){
+	//html에서 attr속성 (data-no)으로 넣어놓은 값 = sidebar item의 인덱스 순서
+	//상단 메뉴 클릭시 sidebar item 연결하기 위함
 	$('#tools .tool').click(function(){
 		$('#tools .tool.active').removeClass('active');
 		var toolname=$(this).attr('data-tool');
@@ -134,8 +94,6 @@ function InitTools(){
 }
 
 
-
-
 function toolClear(){
 	toolSelect=false;
 	toolPipet=false;
@@ -143,8 +101,8 @@ function toolClear(){
 	toolEraser=false;
 	toolMove=false;
 	toolText = false;
-	 $('#text').hide();
-	 isOnText=false;
+	$('#text').hide();
+	isOnText=false;
 
 }
 function SetCurrentTool(toolname){
